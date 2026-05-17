68cb4aef09d509019541a008e42c91f4a953d720
luochen1975 <lzgzg@126.com>
2026-05-17 09:50:25 +0800

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,18 +1,20 @@
+#!/usr/bin/env python3
+"""日志模块 - 北京时间"""
 import logging
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
-# 自定义格式化器，使用北京时间
+
 class BeijingFormatter(logging.Formatter):
+    """自定义格式化器，使用北京时间"""
     def formatTime(self, record, datefmt=None):
-        # 获取北京时间
-        beijing_time = datetime.fromtimestamp(record.created, tz=ZoneInfo('Asia/Shanghai'))
+        beijing_time = datetime.fromtimestamp(record.created, tz=ZoneInfo("Asia/Shanghai"))
         if datefmt:
             return beijing_time.strftime(datefmt)
         return beijing_time.isoformat()
 
-# 使用自定义格式化器
-formatter = BeijingFormatter('%(asctime)s [%(levelname)s] %(message)s', datefmt='%H:%M:%S')
+
+formatter = BeijingFormatter("%(asctime)s [%(levelname)s] %(message)s", datefmt="%H:%M:%S")
 
 handler = logging.StreamHandler()
 handler.setFormatter(formatter)
@@ -20,4 +22,4 @@ handler.setFormatter(formatter)
 log = logging.getLogger("iptv")
 log.setLevel(logging.INFO)
 log.addHandler(handler)
-log.propagate = False  # 避免重复输出
+log.propagate = False
